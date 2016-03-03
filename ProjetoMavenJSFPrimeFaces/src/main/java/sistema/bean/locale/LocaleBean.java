ee923eb9181cfd418f8ac628934611e004f08e3b
teste <Abutua@Abutua3>
2016-03-03 16:55:02 -0300

    Internacionalização da aplicação

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/bean/locale/LocaleBean.java
@@ -0,0 +1,30 @@
+package sistema.bean.locale;
+
+import java.util.Locale;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleBean {
+
+    private Locale locale = Locale.ENGLISH;
+
+  
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+}
