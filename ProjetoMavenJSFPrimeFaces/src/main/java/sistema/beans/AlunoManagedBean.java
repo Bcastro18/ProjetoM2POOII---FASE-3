d38ebcd69d87ca015e5ab9089db32d75d0a3bf9b
teste <Abutua@Abutua3>
2016-03-04 14:57:15 -0300

    Exclusão de pacotes não usados

--- ProjetoMavenJSFPrimeFaces/src/main/java/sistema/beans/AlunoManagedBean.java
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/beans/AlunoManagedBean.java
@@ -1,11 +1,8 @@
 package sistema.beans;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
 import sistema.modelos.Aluno;
 import sistema.service.AlunoService;
 
