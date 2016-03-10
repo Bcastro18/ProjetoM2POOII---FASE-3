2d4278dfad912d9f28601cd000aeba71696646a9
teste <Abutua@Abutua3>
2016-03-10 16:53:00 -0300

    \

--- ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Aluno.java
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Aluno.java
@@ -1,14 +1,21 @@
 package sistema.modelos;
 
+import java.io.Serializable;
+
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Aluno {
+public class Aluno implements Serializable{
 
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int matricula;
+	
 	private String nome;
 	private String curso;
 	
@@ -30,6 +37,32 @@ public class Aluno {
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
+	
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + matricula;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (matricula != other.matricula)
+			return false;
+		return true;
+	}
+	
+	
 	
 
 }
